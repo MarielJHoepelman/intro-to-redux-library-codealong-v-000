18142c8c569db4b9d20af32d128cb352bf38747d
Mariel Hoepelman <marielhoepelman@gmail.com>
2021-02-24 22:44:11 +0000

    Automatically backed up by Learn

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 // ./src/index.js
 
+<<<<<<< HEAD
 import React from "react";
 import ReactDOM from "react-dom";
 import { createStore } from "redux";
@@ -7,6 +8,14 @@ import { Provider } from "react-redux";
 import shoppingListItemReducer from "./reducers/shoppingListItemReducer.js";
 import App from "./App";
 import "./index.css";
+=======
+import React from 'react';
+import ReactDOM from 'react-dom';
+// import { createStore } from 'redux'; /* code change */
+// import shoppingListItemReducer from './reducers/shoppingListItemReducer.js';
+import App from './App';
+import './index.css';
+>>>>>>> 035dd708c124ee616823893a51edb77f69d7610b
 
 const store = createStore(
   shoppingListItemReducer,
